9bcdd8dc8289cc39728f9c496c4aada54c18310d
underdog <underdog@airensoft.com>
2018-07-03 13:51:12 +0900

    Removing unused utils.

--- src/js/utils/strings.js
+++ src/js/utils/strings.js
@@ -38,7 +38,7 @@ export const extractExtension = function(path) {
 
 
 /**
- * Time Formatter
+ * naturalHms
  *
  * @param      {number | string}  second  The second
  * @return     {string}  formatted String
@@ -57,67 +57,4 @@ export function naturalHms(second) {
     } else {
         return minutes+':'+seconds;
     }
-}
-export function hms(secondsNumber) {
-    function pad(str, length, padd) {
-        str = '' + str;
-        padd = padd || '0';
-        while (str.length < length) {
-            str = padd + str;
-        }
-        return str;
-    }
-    let h = parseInt(secondsNumber / 3600);
-    let m = parseInt(secondsNumber / 60) % 60;
-    let s = secondsNumber % 60;
-    return pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s.toFixed(3), 6);
-}
-export function seconds(str, frameRate) {
-    if(!str) {
-        return 0;
-    }
-    if(_.isNumber(str) && !_.isNaN(str)){
-        return str;
-    }
-    str = str.replace(',', '.');
-    let arr = str.split(':');
-    let arrLength = arr.length;
-    let sec = 0;
-    if (str.slice(-1) === 's'){
-        sec = parseFloat(str);
-    }else if (str.slice(-1) === 'm'){
-        sec = parseFloat(str) * 60;
-    }else if (str.slice(-1) === 'h'){
-        sec = parseFloat(str) * 3600;
-    }else if (arrLength > 1) {
-        var secIndex = arrLength - 1;
-        if (arrLength === 4) {
-            if (frameRate) {
-                sec = parseFloat(arr[secIndex]) / frameRate;
-            }
-            secIndex -= 1;
-        }
-        sec += parseFloat(arr[secIndex]);
-        sec += parseFloat(arr[secIndex - 1]) * 60;
-        if (arrLength >= 3) {
-            sec += parseFloat(arr[secIndex - 2]) * 3600;
-        }
-    } else {
-        sec = parseFloat(str);
-    }
-    if (_.isNaN(sec)) {
-        return 0;
-    }
-    return sec;
-}
-
-export function offsetToSeconds(offset, duration, frameRate) {
-    if (_.isString(offset) && offset.slice(-1) === '%') {
-        const percent = parseFloat(offset);
-        if (!duration || isNaN(duration) || isNaN(percent)) {
-            return null;
-        }
-        return duration * percent / 100;
-    }
-    return seconds(offset, frameRate);
 }
