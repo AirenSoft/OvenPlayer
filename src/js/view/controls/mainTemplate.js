1b870f4deac44eee7592e01e614a29b573db12db
underdog <underdog@airensoft.com>
2018-11-13 16:16:28 +0900

    Improved UI animations.

--- src/js/view/controls/mainTemplate.js
+++ src/js/view/controls/mainTemplate.js
@@ -1,8 +1,9 @@
 
 const Controls = function(){
     return '<div class="ovp-controls-container">'+
-         '<div class="ovp-gradient-bottom"></div>' +
+
          '<div class="ovp-bottom-panel">' +
+         '    <div class="ovp-gradient-bottom"></div>' +
          '    <div class="ovp-progressbar-container">' +
          '    </div>' +
          '    <div class="ovp-controls">' +
