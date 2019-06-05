eeb9b1383bbdfa4b5fea806efaf503643db72412
underdoggg <underdog@airensoft.com>
2019-06-05 17:30:13 +0900

    fix : Sometimes enabled rtmp live complete sign. but rtmp live is never complted.

--- dist/development/ovenplayer/ovenplayer.js
+++ dist/development/ovenplayer/ovenplayer.js
@@ -1,4 +1,4 @@
-/*! OvenPlayerv0.9.59561 | (c)2019 AirenSoft Co., Ltd. | MIT license (https://github.com/AirenSoft/OvenPlayerPrivate/blob/master/LICENSE) | Github : https://github.com/AirenSoft/OvenPlayer */
+/*! OvenPlayerv0.9.59562 | (c)2019 AirenSoft Co., Ltd. | MIT license (https://github.com/AirenSoft/OvenPlayerPrivate/blob/master/LICENSE) | Github : https://github.com/AirenSoft/OvenPlayer */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// install a JSONP callback for chunk loading
 /******/ 	function webpackJsonpCallback(data) {
@@ -27756,7 +27756,7 @@ Object.defineProperty(exports, "__esModule", {
 /**
  * Created by hoho on 2018. 6. 29..
  */
-var version = exports.version = '0.9.59561-localbuild';
+var version = exports.version = '0.9.59562-localbuild';
 
 /***/ }),
 
