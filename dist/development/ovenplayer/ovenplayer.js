fb132dc91cdd28f2deb0bff74947298db5b84d2b
Sangwon Oh <rock@airensoft.com>
2020-04-28 10:53:41 +0900

    Bug fixed: make wrong icecandidate when icecandidate does not have ip adress.

--- dist/development/ovenplayer/ovenplayer.js
+++ dist/development/ovenplayer/ovenplayer.js
@@ -14099,7 +14099,7 @@ Object.defineProperty(exports, "__esModule", {
 /**
  * Created by hoho on 2018. 6. 29..
  */
-var version = exports.version = '0.9.0-2020033114-localbuild';
+var version = exports.version = '0.9.0-2020042810-localbuild';
 
 /***/ }),
 
