5f042f96e96b4bb4ea6b18378fc76ae34a496bc8
Sangwon <rock@airensoft.com>
2021-03-02 17:23:28 +0900

    Make context menu simple

--- src/js/view/components/helpers/contextPanelTemplate.js
+++ src/js/view/components/helpers/contextPanelTemplate.js
@@ -2,11 +2,8 @@ import {version} from "version";
 export default (uiText) => {
     return (
         `<div class="op-context-panel animated fadeIn">` +
-            `<div class="op-context-item" tabindex="0">` +
-                `<span class="op-context-item-text">Help</span>` +
-            `</div>`+
             `<div class="op-context-item" tabindex="1">` +
-                `<span class="op-context-item-text">${uiText.context} ${version}</span>` +
+                `<span class="op-context-item-text">${uiText.context}</span>` +
             `</div>`+
         `</div>`
     );
