548443350420e6d248acbf2e915dba2985894f3b
Sangwon Oh <rock@airensoft.com>
2021-10-12 16:49:06 +0900

    Fix the volume button state is not applied when controls:false is set

--- src/js/view/components/controls/volumeButton.js
+++ src/js/view/components/controls/volumeButton.js
@@ -93,6 +93,8 @@ const VolumeButton = function($container, api){
 
         $sliderHandle.css("left", maxRange+ "px");
 
+        setVolumeUI(api.getVolume());
+
         api.on(READY, function() {
             setVolumeUI(api.getVolume());
         }, template);
