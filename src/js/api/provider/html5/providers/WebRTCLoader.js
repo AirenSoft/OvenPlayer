233db1aaea96cd3f56b09fcb17c8cd9c3bb123ba
SangwonOh <rock@airensoft.com>
2021-11-23 13:39:59 +0900

    Change OvenPlayer send answer to OME before setting local description when signaling with OME

--- src/js/api/provider/html5/providers/WebRTCLoader.js
+++ src/js/api/provider/html5/providers/WebRTCLoader.js
@@ -331,19 +331,19 @@ const WebRTCLoader = function (provider, webSocketUrl, loadCallback, errorTrigge
                             }
                         }
 
+                        OvenPlayerConsole.log('Local SDP', desc);
+
+                        sendMessage(ws, {
+                            id: id,
+                            peer_id: peerId,
+                            command: 'answer',
+                            sdp: desc
+                        });
+
                         OvenPlayerConsole.log("create Host Answer : success");
 
                         peerConnection.setLocalDescription(desc).then(function () {
-                            // my SDP created.
-                            let localSDP = peerConnection.localDescription;
-                            OvenPlayerConsole.log('Local SDP', localSDP);
 
-                            sendMessage(ws, {
-                                id: id,
-                                peer_id: peerId,
-                                command: 'answer',
-                                sdp: localSDP
-                            });
 
                         }).catch(function (error) {
 
