3d9ddf0c222f15f9fd8742c274419aa25e6bd121
Sangwon Oh <rock@airensoft.com>
2021-12-08 21:04:14 +0900

    Show the package version on the context panel

--- src/js/view/components/helpers/contextPanelTemplate.js
+++ src/js/view/components/helpers/contextPanelTemplate.js
@@ -3,7 +3,7 @@ export default (uiText) => {
     return (
         `<div class="op-context-panel animated fadeIn">` +
             `<div class="op-context-item" tabindex="1">` +
-                `<span class="op-context-item-text">${uiText.context}</span>` +
+                `<span class="op-context-item-text">${uiText.context} ${version}</span>` +
             `</div>`+
         `</div>`
     );
