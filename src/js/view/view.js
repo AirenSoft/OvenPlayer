6f20855fc62e2fa4e58c47721a80bc1e4ef7a870
Sangwon Oh <rock@airensoft.com>
2021-12-27 21:27:43 +0900

    Fix broken showControls api

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -358,16 +358,7 @@ const View = function($container){
         let showControlBar = api.getConfig() && api.getConfig().controls;
 
         helper = Helpers($playerRoot.find(".op-ui"), playerInstance);
-        if(showControlBar){
-            controls = Controls($playerRoot.find(".op-ui"), playerInstance);
-        } else {
-
-            // to use full screen api
-            if (api.getConfig() && api.getConfig().expandFullScreenUI) {
-                controls = Controls($playerRoot.find(".op-ui"), playerInstance);
-                controls.destroy();
-            }
-        }
+        controls = Controls($playerRoot.find(".op-ui"), playerInstance);
 
         let aspectRatio = api.getConfig().aspectRatio;
 
