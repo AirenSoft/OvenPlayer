fe649fea77fffd60864988a8acbc8e57f8c8b4d5
billy <billy@example.com>
2023-10-23 12:08:34 +0800

    [vue3 component] fix: types hint for bundler
    bundler read different field in package.json.
    
    @SangwonOh, new package release is required after this pr get mreged.
    
    Thank you

--- packages/vue3/dist/ovenplayer-vue3.es.js
+++ packages/vue3/dist/ovenplayer-vue3.es.js
@@ -1,14 +1,14 @@
-import { defineComponent as t, ref as v, onMounted as C, onUnmounted as c, onUpdated as g, openBlock as y, createElementBlock as p } from "vue";
-import s from "ovenplayer";
-const k = /* @__PURE__ */ t({
+import { defineComponent as h, ref as v, onMounted as i, onUnmounted as c, onUpdated as C, openBlock as g, createElementBlock as m } from "vue";
+import y from "ovenplayer";
+const k = /* @__PURE__ */ h({
   __name: "OvenPlayer",
   props: {
     config: {}
   },
   emits: ["ready", "metaChanged", "stateChanged", "resized", "playbackRateChanged", "seek", "time", "bufferChanged", "mute", "volumeChanged", "playlistChanged", "sourceChanged", "qualityLevelChanged", "cueChanged", "timeDisplayModeChanged", "adChanged", "adTime", "adComplete", "fullscreenChanged", "clicked", "allPlaylistEnded", "hlsPrepared", "hlsDestroyed", "dashPrepared", "dashDestroyed", "destroy", "error"],
-  setup(r, { expose: u, emit: n }) {
-    const h = r, a = v(), o = "ovenplayer-" + Math.random().toString().replace(".", ""), d = () => {
-      a.value = s.create(o, h.config), a.value.on("ready", () => n("ready")), a.value.on("metaChanged", (e) => n("metaChanged", e)), a.value.on("stateChanged", (e) => n("stateChanged", e)), a.value.on("resized", (e) => n("resized", e)), a.value.on("playbackRateChanged", (e) => n("playbackRateChanged", e)), a.value.on("seek", (e) => n("seek", e)), a.value.on("time", (e) => n("time", e)), a.value.on("bufferChanged", (e) => n("bufferChanged", e)), a.value.on("mute", (e) => n("mute", e)), a.value.on("volumeChanged", (e) => n("volumeChanged", e)), a.value.on("playlistChanged", (e) => n("playlistChanged", e)), a.value.on("sourceChanged", (e) => n("sourceChanged", e)), a.value.on("qualityLevelChanged", (e) => n("qualityLevelChanged", e)), a.value.on("cueChanged", (e) => n("cueChanged", e)), a.value.on("timeDisplayModeChanged", (e) => n("timeDisplayModeChanged", e)), a.value.on("adChanged", (e) => n("adChanged", e)), a.value.on("adTime", (e) => n("adTime", e)), a.value.on("adComplete", () => n("adComplete")), a.value.on("fullscreenChanged", (e) => n("fullscreenChanged", e)), a.value.on("clicked", (e) => n("clicked", e)), a.value.on("allPlaylistEnded", () => n("allPlaylistEnded")), a.value.on("hlsPrepared", (e) => n("hlsPrepared", e)), a.value.on("hlsDestroyed", () => n("hlsDestroyed")), a.value.on("dashPrepared", (e) => n("dashPrepared", e)), a.value.on("dashDestroyed", () => n("dashDestroyed")), a.value.on("destroy", () => n("destroy")), a.value.on("error", (e) => n("error", e));
+  setup(r, { expose: t, emit: s }) {
+    const a = v(), o = "ovenplayer-" + Math.random().toString().replace(".", ""), u = r, n = s, d = () => {
+      a.value = y.create(o, u.config), a.value.on("ready", () => n("ready")), a.value.on("metaChanged", (e) => n("metaChanged", e)), a.value.on("stateChanged", (e) => n("stateChanged", e)), a.value.on("resized", (e) => n("resized", e)), a.value.on("playbackRateChanged", (e) => n("playbackRateChanged", e)), a.value.on("seek", (e) => n("seek", e)), a.value.on("time", (e) => n("time", e)), a.value.on("bufferChanged", (e) => n("bufferChanged", e)), a.value.on("mute", (e) => n("mute", e)), a.value.on("volumeChanged", (e) => n("volumeChanged", e)), a.value.on("playlistChanged", (e) => n("playlistChanged", e)), a.value.on("sourceChanged", (e) => n("sourceChanged", e)), a.value.on("qualityLevelChanged", (e) => n("qualityLevelChanged", e)), a.value.on("cueChanged", (e) => n("cueChanged", e)), a.value.on("timeDisplayModeChanged", (e) => n("timeDisplayModeChanged", e)), a.value.on("adChanged", (e) => n("adChanged", e)), a.value.on("adTime", (e) => n("adTime", e)), a.value.on("adComplete", () => n("adComplete")), a.value.on("fullscreenChanged", (e) => n("fullscreenChanged", e)), a.value.on("clicked", (e) => n("clicked", e)), a.value.on("allPlaylistEnded", () => n("allPlaylistEnded")), a.value.on("hlsPrepared", (e) => n("hlsPrepared", e)), a.value.on("hlsDestroyed", () => n("hlsDestroyed")), a.value.on("dashPrepared", (e) => n("dashPrepared", e)), a.value.on("dashDestroyed", () => n("dashDestroyed")), a.value.on("destroy", () => n("destroy")), a.value.on("error", (e) => n("error", e));
     }, l = () => {
       if (a.value) {
         try {
@@ -18,13 +18,13 @@ const k = /* @__PURE__ */ t({
         a.value = void 0;
       }
     };
-    return u({
+    return t({
       createPlayer: d,
       removePlayer: l,
       playerInstance: a
-    }), C(d), c(l), g(() => {
+    }), i(d), c(l), C(() => {
       l(), d();
-    }), (e, f) => (y(), p("div", { id: o }));
+    }), (e, p) => (g(), m("div", { id: o }));
   }
 });
 export {
